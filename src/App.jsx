f44a235496d88159e56f29850faa5099437bba18
yarkhan706 <yarkhan706@gmail.com>
2023-08-09 15:16:26 +0500

    BASIC LAYOUT FOR STORE AND SETTING UP FILE

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './categories.styles.scss'
+
+const App = () => {
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 4, title: "Womens" },
+    { id: 5, title: "Mens" },
+  ];
+  return (
+    <div className="categories-container">
+      {categories.map(({title , id}) => (
+        <div key={id} className="category-container">
+          <div className="category-body-container">
+            {/* <img src="" alt="" /> */}
+            <h2>{title}</h2>
+            <p>SHOP NOW!</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
