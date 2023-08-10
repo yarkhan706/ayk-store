82a793893a87f94ec1f2f6adbb6ca790c50a0c07
yarkhan706 <yarkhan706@gmail.com>
2023-08-10 15:48:51 +0500

    ROUTE OUTLET structure studying and index default value is true and how to use outlets

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { Routes , Route } from "react-router-dom";
+import { Routes , Route , Outlet} from "react-router-dom";
 import Home from "./routes/home/home.component";
+
+const Navigation = () =>
+{
+  return (
+    <div>
+      <div>
+        <h1>I am navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+    
+  );
+}
 
 const Shop = () =>( <h2>I AM SHOP PAGE</h2>)
 
 const App = () => {
   return(
     <Routes>
-        <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}> 
+
+          <Route index = {true} element={<Home />} />
           <Route path="shop" element={<Shop/>}/>
-          </Route>
+      </Route>
+          
     </Routes>
   )
 };
