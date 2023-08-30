51d5c9cf44959dceb99a7cee7fe94c30972294da
yarkhan706 <yarkhan706@gmail.com>
2023-08-30 12:50:50 +0500

    CartIcon value update

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,12 +5,12 @@ import { CartContext } from '../../context/cart.context';
 
 const CartIcon = () =>
 {
-    const {isCartOpen,setIsCartOpen} = useContext(CartContext);
+    const {isCartOpen,setIsCartOpen,cartCount} = useContext(CartContext);
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
     return(
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingCart className='shopping-icon'/>
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     );
 }
