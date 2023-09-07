5730d298ba220129b1cfb3604234fc98dd367a88
yarkhan706 <yarkhan706@gmail.com>
2023-09-07 08:15:23 +0500

    checkout route and checkout item component

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,18 @@
+import "./checkout-item.styles.scss";
+
+const CheckOutItem = ({ cartitem }) => {
+  const { name, imageUrl, price, quantity } = cartitem;
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={`${name}`} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button">&#10005;</div>
+    </div>
+  );
+};
+
+export default CheckOutItem;
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,25 +1,25 @@
 import "./checkout.styles.scss";
 import { CartContext } from "../../context/cart.context";
 import { useContext, useEffect } from "react";
+import CheckOutItem from "../../components/checkout-item/checkout-item.component";
 
 const CheckOut = () => {
-  const { cartItems,addItemToCart , removeItemFromCart} = useContext(CartContext);
+  const { cartItems, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
   return (
-    <div>
-      {cartItems.map((item) => {
-        const { id, name, quantity, imageUrl, price } = item;
-        return (
-          <div key={id}>
-            <img src={imageUrl} alt={`${name}`} />
-            <h2>{name}</h2>
-            <span onClick={() => removeItemFromCart(item)}>&lt;</span>
-            <span>{quantity}</span>
-            <span onClick={() => addItemToCart(item)}>&gt;</span>
-            <br />
-            <span>{price}</span>
-          </div>
-        );
-      })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">PRODUCT</div>
+        <div className="header-block">DESCRIPTION</div>
+        <div className="header-block">QUANTITY</div>
+        <div className="header-block">PRICE</div>
+        <div className="header-block">REMOVE</div>
+      </div>
+
+      {cartItems.map((item) => (
+        <CheckOutItem key={item.id} cartitem={item} />
+      ))}
+      <span className="total">Total: 0</span>
     </div>
   );
 };
