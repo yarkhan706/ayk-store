813fdee6790d2cd441d97e3e3ac77cc74decc128
yarkhan706 <yarkhan706@gmail.com>
2023-09-07 19:24:23 +0500

    ADDING REMOVE FUNCTIONALITY

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,34 @@
+import { useContext } from "react";
 import "./checkout-item.styles.scss";
+import { CartContext } from "../../context/cart.context";
 
 const CheckOutItem = ({ cartitem }) => {
   const { name, imageUrl, price, quantity } = cartitem;
+  const { clearItemFromCart, addItemToCart, removeItemFromCart } =
+    useContext(CartContext);
+  const cartItemToClearHandler = () => clearItemFromCart(cartitem);
+  const addItemToCartHandler = () => addItemToCart(cartitem);
+  const removeItemFromCartHandler = () => removeItemFromCart(cartitem);
   return (
     <div className="checkout-item-container">
       <div className="image-container">
         <img src={imageUrl} alt={`${name}`} />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={removeItemFromCartHandler}>
+          {" "}
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={addItemToCartHandler}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={cartItemToClearHandler}>
+        &#10005;
+      </div>
     </div>
   );
 };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,16 +4,25 @@ import { useContext, useEffect } from "react";
 import CheckOutItem from "../../components/checkout-item/checkout-item.component";
 
 const CheckOut = () => {
-  const { cartItems, addItemToCart, removeItemFromCart } =
-    useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="checkout-container">
       <div className="checkout-header">
-        <div className="header-block">PRODUCT</div>
-        <div className="header-block">DESCRIPTION</div>
-        <div className="header-block">QUANTITY</div>
-        <div className="header-block">PRICE</div>
-        <div className="header-block">REMOVE</div>
+        <div className="header-block">
+          <span>PRODUCT</span>
+        </div>
+        <div className="header-block">
+          <span>DESCRIPTION</span>
+        </div>
+        <div className="header-block">
+          <span>QUANTITY</span>
+        </div>
+        <div className="header-block">
+          <span>PRICE</span>
+        </div>
+        <div className="header-block">
+          <span>REMOVE</span>
+        </div>
       </div>
 
       {cartItems.map((item) => (
